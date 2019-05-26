d9e0d1d1e606b33fd6e58294aba6948cb365cb59
Arturo Puente <arturo@able.co>
2019-05-25 23:53:12 -0500

    Print dictionary of character count

--- keyboard-stats.py
+++ keyboard-stats.py
@@ -1,11 +1,25 @@
 import os
 import sys
 
-root_dir = sys.argv[1]
-directories = open('./directories.txt').read().splitlines()
+def read(f):
+    return open(f).read().splitlines()
 
-for dirpath, dirnames, files in os.walk(root_dir):
-    for directory in directories:
-        if dirpath.replace(root_dir + '/', '').startswith(directory):
-            print(dirpath)
-            print(files)
+root_dir = sys.argv[1]
+directories = read('./directories.txt')
+filetypes = read('./filetypes.txt')
+characters = read('./characters.txt')
+
+def run():
+    results = {key: 0 for key in characters}
+    for dirpath, dirnames, files in os.walk(root_dir):
+        for directory in directories:
+            if dirpath.replace(root_dir + '/', '').startswith(directory):
+                for file in files:
+                    if os.path.splitext(file)[1] in filetypes:
+                        for line in open(f'{dirpath}/{file}').readlines():
+                            for char in characters:
+                                results[char] = results[char] + line.count(char)
+    return results
+
+data = run()
+print(data)
